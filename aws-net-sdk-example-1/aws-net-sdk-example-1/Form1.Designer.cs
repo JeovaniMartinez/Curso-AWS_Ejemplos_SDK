e62f41376a71821a82a3117bdfba5d2470524f18
Jeovani Martínez <jeovani_martinez@hotmail.com>
2021-08-11 18:01:09 -0500

    Se agrega ejemplo para .NET

---
+++ aws-net-sdk-example-1/aws-net-sdk-example-1/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace aws_net_sdk_example_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbSubject = new System.Windows.Forms.TextBox();
+            this.tbDestination = new System.Windows.Forms.TextBox();
+            this.rtbContent = new System.Windows.Forms.RichTextBox();
+            this.btSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Asunto";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(11, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Destinatario";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(218, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Mensaje";
+            // 
+            // tbSubject
+            // 
+            this.tbSubject.Location = new System.Drawing.Point(116, 18);
+            this.tbSubject.Name = "tbSubject";
+            this.tbSubject.Size = new System.Drawing.Size(375, 20);
+            this.tbSubject.TabIndex = 3;
+            // 
+            // tbDestination
+            // 
+            this.tbDestination.Location = new System.Drawing.Point(116, 70);
+            this.tbDestination.Name = "tbDestination";
+            this.tbDestination.Size = new System.Drawing.Size(375, 20);
+            this.tbDestination.TabIndex = 4;
+            // 
+            // rtbContent
+            // 
+            this.rtbContent.AcceptsTab = true;
+            this.rtbContent.Location = new System.Drawing.Point(14, 165);
+            this.rtbContent.Name = "rtbContent";
+            this.rtbContent.Size = new System.Drawing.Size(477, 142);
+            this.rtbContent.TabIndex = 5;
+            this.rtbContent.Text = "";
+            // 
+            // btSend
+            // 
+            this.btSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btSend.Location = new System.Drawing.Point(221, 347);
+            this.btSend.Name = "btSend";
+            this.btSend.Size = new System.Drawing.Size(76, 35);
+            this.btSend.TabIndex = 6;
+            this.btSend.Text = "Enviar";
+            this.btSend.UseVisualStyleBackColor = true;
+            this.btSend.Click += new System.EventHandler(this.btSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(505, 403);
+            this.Controls.Add(this.btSend);
+            this.Controls.Add(this.rtbContent);
+            this.Controls.Add(this.tbDestination);
+            this.Controls.Add(this.tbSubject);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Correo Electrónico";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbSubject;
+        private System.Windows.Forms.TextBox tbDestination;
+        private System.Windows.Forms.RichTextBox rtbContent;
+        private System.Windows.Forms.Button btSend;
+    }
+}
+
---
+++ aws-net-sdk-example-1/aws-net-sdk-example-1/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+using Amazon;
+using Amazon.SimpleEmail;
+using Amazon.SimpleEmail.Model;
+
+
+namespace aws_net_sdk_example_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Realiza el envió de un correo electrónico mediante AWS, con los valores especificados en los parámetros
+        private void sendEmail(String source, String destination, String subject, String content)
+        {
+            using (var client = new AmazonSimpleEmailServiceClient(RegionEndpoint.USEast1))
+            {
+                var sendRequest = new SendEmailRequest
+                {
+                    Source = source,
+                    Destination = new Destination
+                    {
+                        ToAddresses =
+                        new List<string> { destination }
+                    },
+                    Message = new Amazon.SimpleEmail.Model.Message
+                    {
+                        Subject = new Content(subject),
+                        Body = new Body
+                        {
+                            Text = new Content
+                            {
+                                Charset = "UTF-8",
+                                Data = content
+                            }
+                        }
+                    },
+                };
+                try
+                {
+                    var response = client.SendEmail(sendRequest);
+                    MessageBox.Show("El correo se ha enviado correctamente");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void btSend_Click(object sender, EventArgs e)
+        {
+            sendEmail("contacto@app-notas.com", tbDestination.Text, tbSubject.Text, rtbContent.Text);
+        }
+    }
+}
